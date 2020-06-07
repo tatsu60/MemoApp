0a782e3d1aaf0a573fea57096028e63bda893e87
佐藤 達郎 <satotatsuro@satou-no-MacBook-Air.local>
2020-06-07 13:39:06 +0900

    Settings for eslint again

--- src/screens/MemoDetailScreen.js
+++ src/screens/MemoDetailScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native' ;
+import { StyleSheet, Text, View } from 'react-native';
 
 import CircleButton from '../elements/CircleButton';
 
