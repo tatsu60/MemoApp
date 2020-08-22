a401fae8099e5f87935e1c41a5bc0a8d0df47ccc
佐藤 達郎 <satotatsuro@satou-no-MacBook-Air.local>
2020-08-22 12:51:41 +0900

    Add AutomaticLogin to LoginScreen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,28 +1,49 @@
 import React from 'react';
+import * as SecureStore from 'expo-secure-store';
 import { StyleSheet, View, Text, TextInput, TouchableHighlight, TouchableOpacity } from 'react-native';
 import firebase from 'firebase';
 import { StackActions, NavigationActions } from 'react-navigation';
+import Loading from '../elements/Loading';
 
 class LoginScreen extends React.Component {
   state = {
-    email: 'user1@example.com',
-    password: 'password',
+    email: '',
+    password: '',
+    isLoading: true,
+  }
+
+  async componentDidMount() {
+    const email = await SecureStore.getItemAsync('email');
+    const password = await SecureStore.getItemAsync('password');
+    if (email && password) {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(() => {
+          this.setState({ isLoading: false });
+          this.navigateToHome();
+        })
+        .catch();
+    }
+  }
+
+  navigateToHome() {
+    const resetAction = StackActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'Home' }),
+      ],
+    });
+    this.props.navigation.dispatch(resetAction);
   }
 
   // eslint-disable-next-line
   handleSubmit() {
     firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
       .then(() => {
-        const resetAction = StackActions.reset({
-          index: 0,
-          actions: [
-            NavigationActions.navigate({ routeName: 'Home' }),
-          ],
-        });
-        this.props.navigation.dispatch(resetAction);
+        SecureStore.setItemAsync('email', this.state.email);
+        SecureStore.setItemAsync('password', this.state.password);
+        this.navigateToHome();
       })
-      .catch(() => {
-      });
+      .catch();
   }
 
   handlePress() {
@@ -32,6 +53,7 @@ class LoginScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Loading text="ログイン中" isLoading={this.state.isLoading} />
         <Text style={styles.title}>
           ログイン
         </Text>
