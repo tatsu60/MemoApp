cb6cf7301410fcb5753a31e56d8db7e01c5ac2ad
佐藤 達郎 <satotatsuro@satou-no-MacBook-Air.local>
2020-08-22 13:05:04 +0900

    Fix button position bug

--- src/screens/MemoEditScreen.js
+++ src/screens/MemoEditScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, TextInput } from 'react-native';
 
 import firebase from 'firebase';
 
@@ -44,7 +44,7 @@ class MemoEditScreen extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior="height" keyboardVerticalOffset={70}>
         <TextInput
           style={styles.memoEditInput}
           multiline
@@ -56,7 +56,7 @@ class MemoEditScreen extends React.Component {
         />
 
         <CircleButton name="check" onPress={this.handlePress.bind(this)} />
-      </View>
+      </KeyboardAvoidingView>
     );
   }
 }
